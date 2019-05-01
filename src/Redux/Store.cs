343f49842c304f15043383094e542c5645bb7e17
afm <a.fouad@defectradar.com>
2019-05-02 00:52:54 +0200

    Add error listner

--- src/Redux/Store.cs
+++ src/Redux/Store.cs
@@ -16,6 +16,19 @@ namespace Redux
 
         TState GetState();
     }
+
+    public class Error
+    {
+        public Error(IAction action, Exception exception)
+        {
+            Action = action;
+            Exception = exception;
+        }
+
+        public IAction Action { get; }
+        public Exception Exception { get; }
+
+    }
     public class AsyncStore<TState> : Store<Task<TState>>
     {
         public AsyncStore(Reducer<Task<TState>> reducer, Task<TState> initialState = null)
@@ -25,13 +38,18 @@ namespace Redux
         public Task DispatchAsync(IAction action)
         {
             var lastState = _reducer.Invoke(_lastState, action);
-            lastState.ContinueWith(task => _lastState = task, TaskContinuationOptions.OnlyOnRanToCompletion);
+            lastState.ContinueWith(task =>
+            {
+                _lastState = task;
+                _stateSubject.OnNext(_lastState);
 
-            return lastState;
-            _lastState = _reducer.Invoke(_lastState, action);
-            _stateSubject.OnNext(_lastState);
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            lastState.ContinueWith(task =>
+            {
+                _faultedSubject.OnNext(new Error(action,task.Exception));
 
-            return _lastState;
+            }, TaskContinuationOptions.NotOnRanToCompletion);
+            return lastState;            
         }
     }
     public class Store<TState> : IStore<TState>
@@ -40,6 +58,8 @@ namespace Redux
         private readonly Dispatcher _dispatcher;
         protected readonly Reducer<TState> _reducer;
         protected readonly ReplaySubject<TState> _stateSubject = new ReplaySubject<TState>(1);
+        protected readonly ReplaySubject<Error> _faultedSubject = new ReplaySubject<Error>(1);
+
         protected TState _lastState;
 
         public Store(Reducer<TState> reducer, TState initialState = default(TState), params Middleware<TState>[] middlewares)
@@ -66,7 +86,11 @@ namespace Redux
             return _stateSubject
                 .Subscribe(observer);
         }
-
+        public IDisposable SubscribeForErrors(IObserver<Error> observer)
+        {
+            return _faultedSubject
+                .Subscribe(observer);
+        }
         private Dispatcher ApplyMiddlewares(params Middleware<TState>[] middlewares)
         {
             Dispatcher dispatcher = InnerDispatch;
